39706c4fbb1c2f295246a28010c204b253d192a2
Max Lettenberger <maxlettenberger@gmail.com>
2022-05-02 23:55:25 -0400

    adding redux to calc

--- src/components/StockCalCulator.js
+++ src/components/StockCalCulator.js
@@ -1,32 +1,63 @@
 import React, { useState, useEffect } from "react";
 import { useStocks, useStocksUpdate } from "../context/StocksContext";
+import { useCounter } from "../hooks/useCounter";
+import { mystore } from "../MyRedux/MyRedux";
+import {
+  myconnect,
+  useMySelector,
+  useMyDispatch,
+} from "../MyReactRedux/MyReactRedux";
+import { useForceUpdate } from "../hooks/useForceUpdate";
 
 class StockCalCulator extends React.Component {
+  changeVal(e) {
+    this.props.counterValue = Number(e.target.value)
+  }
   render() {
     return (
       <section>
         <h1>Calculator</h1>
         <div>How many stonks do you have?</div>
-        <input type="number" />
+        <input value={this.props.counterValue} onChange={this.changeVal} type="number" />
         <div>How many stonks would you like to buy?</div>
-        <button type="button">Buy + </button> {0}
-        <button type="button">Sell -</button>
+        <button onClick={() => {
+            this.props.hanldeAdd();
+          }} name="buy" type="button">Buy + </button> {0}
+        <button onClick={() => {
+            this.props.hanldeSub();
+          }} name="sell" type="button">Sell -</button>
         <div>you will have 0 stonks after your purchase</div>
       </section>
     );
   }
 }
 
-export default StockCalCulator;
+const mapStateToProps = (state) => {
+  return {
+    counterValue: state.value,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    hanldeAdd: () => dispatch({ type: 'COUNTER_ADD' }),
+    hanldeSub: () => dispatch({ type: 'COUNTER_SUB' }),
+  };
+};
+
+const storeConnect = myconnect(mapStateToProps, mapDispatchToProps);
+
+export default storeConnect(StockCalCulator);
+
 
 export const StockCalCulatorFn = () => {
   let stocks = useStocks();
-  let updateStocks = useStocksUpdate();
+  const updateStocks = useStocksUpdate();
 
   const [order, setOrder] = useState(0);
 
   const updateStocksFn = (e) => {
-    let newStocksCount = Number(e.target.value);
+    const newStocksCount = Number(e.target.value);
     updateStocks(newStocksCount)
   }
 
@@ -41,7 +72,7 @@ export const StockCalCulatorFn = () => {
     <section>
       <h1>Calculator</h1>
       <div>How many stonks do you have?</div>
-      <input placeholder={stocks} type="number" onChange={updateStocksFn}/>
+      <input value={stocks} type="number" onChange={updateStocksFn}/>
       <div>How many stonks would you like to buy?</div>
       <button onClick={addOrder} type="button">
         Buy +{" "}
